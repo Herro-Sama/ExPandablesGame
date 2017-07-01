a4d5eed1e78b93f8385c89e823e981b88c8b9a8a
mooseyballs <troydatkinson@gmail.com>
2017-07-01 23:06:32 +0100

    Add files via upload

--- Assets/Scripts/PandaPatrol.cs
+++ Assets/Scripts/PandaPatrol.cs
@@ -17,11 +17,21 @@ public class PandaPatrol : MonoBehaviour
     [SerializeField]
     private float walkSpeed = 1f;
 
+    [SerializeField]
+    private float turnSpeed = 300f;
+
     private float counter;
     private float nextWaitTime;
 
+    private IEnumerator turnRight;
+    private IEnumerator turnLeft;
+
+    private Animator animator;
+
     private void Awake()
     {
+        animator = transform.GetChild(0).GetComponent<Animator>();
+
         maxLeft = transform.parent.GetChild(1).position.x;
         maxRight = transform.parent.GetChild(2).position.x;
 
@@ -30,6 +40,9 @@ public class PandaPatrol : MonoBehaviour
         movingRight = true;
         still = false;
         nextWaitTime = (Random.Range(minWaitTime*100, maxWaitTime*100))/100;
+
+        turnLeft = TurnToLeft(transform.GetChild(0));
+        turnRight = TurnToRight(transform.GetChild(0));
     }
 
     private void Update()
@@ -39,23 +52,30 @@ public class PandaPatrol : MonoBehaviour
         {
             Flip();
             counter = 0;
-            nextWaitTime = (Random.Range(minWaitTime * 100, maxWaitTime * 100)) / 100;
+            nextWaitTime = (Random.Range(minWaitTime * 100, (still ? maxWaitTime/2 : maxWaitTime) * 100)) / 100;
         }
 
         if (!still)
             transform.Translate((movingRight ? Vector3.right : Vector3.left) * Time.deltaTime * walkSpeed);
-
-
-        Debug.Log(transform.position);
         
         if(movingRight && transform.position.x > maxRight)
         {
             movingRight = false;
+            StopCoroutine(turnLeft);
+            StopCoroutine(turnRight);
+
+            turnLeft = TurnToLeft(transform.GetChild(0));
+            StartCoroutine(turnLeft);
         }
         
         else if (transform.position.x < maxLeft)
         {
             movingRight = true;
+            StopCoroutine(turnLeft);
+            StopCoroutine(turnRight);
+
+            turnRight = TurnToRight(transform.GetChild(0));
+            StartCoroutine(turnRight);
         }
         
     }
@@ -63,5 +83,31 @@ public class PandaPatrol : MonoBehaviour
     private void Flip()
     {
         still = !still;
+        if(still)
+        {
+            animator.SetBool("isWalking", false);
+        }
+        else
+        {
+            animator.SetBool("isWalking", true);
+        }
+    }
+
+    private IEnumerator TurnToRight(Transform tran)
+    {
+        while (tran.eulerAngles.y > 90)
+        {
+            tran.Rotate(0, -(Time.deltaTime * turnSpeed), 0);
+            yield return null;
+        }
+    }
+
+    private IEnumerator TurnToLeft(Transform tran)
+    {
+        while (tran.eulerAngles.y < 270)
+        {
+            tran.Rotate(0, (Time.deltaTime * turnSpeed), 0);
+            yield return null;
+        }
     }
 }
